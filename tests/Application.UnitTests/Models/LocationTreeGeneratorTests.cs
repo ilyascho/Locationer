3ff4bdd6e118b063781a91dc49c398ac2bd4391f
ilyascho <dj_ilyas@yahoo.com>
2021-04-13 17:29:48 +0300

    Update 2

--- tests/Application.UnitTests/Models/LocationTreeGeneratorTests.cs
+++ tests/Application.UnitTests/Models/LocationTreeGeneratorTests.cs
@@ -4,6 +4,7 @@ using Application.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Application.UnitTests.Models
@@ -21,5 +22,31 @@ namespace Application.UnitTests.Models
 
             Assert.IsNotNull(locationTree);
         }
+
+        /// <summary>
+        /// Test method for LocationTree.Generator.Generate And LocationTree.Search
+        /// </summary>
+        [TestMethod()]
+        public void GenerateAndSearchLocationTree()
+        {
+            string filePath = @"D:\Locationer\Locationer\src\Infrastructure\Data\Excel\locations.csv";
+            LocationDto originLocation = new LocationDto("LocationA", 52.216542, 5.4778534);
+            int maxDistance = 1000;
+            int maxResults = 500;
+            int expectedCount = 124;
+
+            //Act
+            LocationTree locationTree = LocationTreeGenerator.GenerateFromExcel(originLocation.Latitude, originLocation.Longitude, filePath);
+
+            var searchResult = locationTree.Search(maxDistance, maxResults)
+                                .OrderBy(o => o.Distance).Take(maxResults);
+
+            List<LocationDto> locations = searchResult.ToList();
+            int actualCount = locations.Count;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
     }
 }
